e0b4bd3fa50a583872bfac2b4f819499c52172a2
Silviya Slavcheva <s.slavcheva@dynamosoftware.com>
2023-06-13 17:34:08 +0300

    cojc

--- dynamo_api_perf_tests2.ps1
+++ dynamo_api_perf_tests2.ps1
@@ -1,5 +1,5 @@
 
-& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\bin\Perf_API.jmx" -l "C:\BuildAgent\work\39fc5dc3e607d09c\Logs.jtl" -g "C:\BuildAgent\work\39fc5dc3e607d09c\Logs.jtl" -о "C:\Users\s.slavcheva\Desktop\Demo\Report"
+& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\bin\Perf_API.jmx" -l "C:\BuildAgent\work\39fc5dc3e607d09c\Logs.jtl"
 
 
 
