23123b78b93be94e128ea32ad0bc6278c94ac123
Silviya Slavcheva <s.slavcheva@dynamosoftware.com>
2023-06-13 17:25:47 +0300

    change path

--- dynamo_api_perf_tests2.ps1
+++ dynamo_api_perf_tests2.ps1
@@ -1,5 +1,5 @@
 
-& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\apache-jmeter-5.5\bin\Perf_API.jmx" -l "C:\BuildAgent\work\808e2429dfa6b1e6\Logs.jtl" -g "C:\BuildAgent\work\39fc5dc3e607d09c\Logs.jtl" -о "C:\Users\s.slavcheva\Desktop\Demo\Report"
+& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDynamo\apache-jmeter-5.5\apache-jmeter-5.5\bin\Perf_API.jmx" -l "C:\BuildAgent\work\39fc5dc3e607d09c\Logs.jtl" -g "C:\BuildAgent\work\39fc5dc3e607d09c\Logs.jtl" -о "C:\Users\s.slavcheva\Desktop\Demo\Report"
 
 
 
